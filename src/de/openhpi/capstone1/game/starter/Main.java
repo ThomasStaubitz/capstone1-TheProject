787c24319fcfc541d91c9a9e4327f767a2bd472b
Buecherwurn <info@pcdienstleister.de>
2018-09-14 21:49:31 +0200

    Fixed the Missing Buildpath to core3.java and imported core.jar into project

--- src/de/openhpi/capstone1/game/starter/Main.java
+++ src/de/openhpi/capstone1/game/starter/Main.java
@@ -1,12 +1,13 @@
 package de.openhpi.capstone1.game.starter;
 
-import processing.core.PApplet;
+	import processing.core.PApplet;
 
 public class Main {
 
 	public static void main(String[] args) {
-		PApplet.main(new String[]{TheApp.class.getName()});
-//		PApplet.main(new String[]{"--present",TheStopwatch.class.getName()});
+	PApplet.main(new String[]{TheApp.class.getName()});
+//	PApplet.main(new String[]{"--present",TheStopwatch.class.getName()});
+	System.out.println("Running so far");
 	}
 
 }
